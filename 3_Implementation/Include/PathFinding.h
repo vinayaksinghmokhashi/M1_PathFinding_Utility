9e04c0d92bb0adc6645dd2d809f54765e9e7a1d0
vinayaksinghmokhashi <98843684+vinayaksinghmokhashi@users.noreply.github.com>
2022-03-03 18:24:24 +0530

    Add files via upload

---
+++ 3_Implementation/Include/PathFinding.h
@@ -0,0 +1,11 @@
+#ifndef PathFinding_H_
+#define PathFinding_H_
+#include<stdio.h>
+
+int ary[10][10],completed[10],n,cost;
+int findpath(int n);
+float passObjectnearest(int e);
+int takeInput();
+int mincost(int computer);
+int least(int c);
+#endif
